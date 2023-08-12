15c38b67bb82578f87fb2ece856d7a88dcf8655f
Thiago Luz <thiago.silva@ifg.edu.br>
2023-08-12 16:16:54 -0300

    Aula 01

---
+++ src/test/java/com/deliverygdg/AppDelivery/AppDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverygdg.AppDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
